aacabf1f083660f1b38d6ac4a23059379005c898
lulukakarara775-hub <lulukakarara775@gmail.com>
2025-11-19 14:48:18 +0300

    Add files via upload

---
+++ практическая 1.3.py
@@ -0,0 +1,33 @@
+num_1 = 7
+num_2 = 10
+num_3 = 4
+
+sum_all = num_1 + num_2 + num_3
+print("Сумма всех чисел:", sum_all)
+
+num_1 = 7.9
+num_2 = 21.3
+num_3 = float(num_3)
+sum_all = num_1 + num_2 + num_3
+print("Сумма всех нецелых чисел:", sum_all)
+
+num_1 = 130
+num_2 = 4
+num_3 = 2
+multiplying = num_1 * num_2 * num_3
+print("Умножение всех чисел:", multiplying)
+division = (num_1 / num_2) / num_3
+print("Деление:", division)
+
+num_1 = 2
+num_2 = 3
+num_3 = 4
+S1 = (num_1 ** num_2) ** num_3
+degree = S1
+print("Числа в степени:", degree)
+
+num_1 = 2
+num_2 = 8
+num_3 = 5
+math = ((43 + num_1) + (num_2 + 67) - (num_3 * 2)) // 2 
+print("Результат выражения:", math)
