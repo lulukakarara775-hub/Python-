f2efd37148509a7a2c56c244a76d18178119362f
lulukakarara775-hub <lulukakarara775@gmail.com>
2025-11-19 14:17:07 +0300

    Add files via upload

---
+++ практическая 1.1.py
@@ -0,0 +1,23 @@
+first_num = 9
+second_num = 7.8
+my_str = "start"
+print(first_num, second_num, my_str)
+
+first_num= 5.2
+print(first_num, type (first_num))
+
+third_num = first_num + second_num
+print(third_num, type(third_num))
+
+first_num += 5
+second_num += first_num
+print(first_num, second_num)
+
+second_num = int(second_num)
+print(first_num, second_num)
+
+my_str += "&stop"
+print(my_str)
+
+my_str *= 5
+print(my_str)
---
+++ практическая 1.2.py
@@ -0,0 +1,14 @@
+path = "C:\\User\\MainAdmin\\Deskpot\\programs"
+print(path)
+
+path += "\\game.exe"
+print(path)
+
+path = "C:\\User\\MainAdmin\\Deskpot\\files"
+path += "\\pictyre.png"
+print(path)
+
+path = "C:\\Games\\city simulator"
+path *= 2
+print(path)
+print("Error path:", path)
